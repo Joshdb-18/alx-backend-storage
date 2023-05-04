c9eb2a6198a5ab30ca062e58a7b20f4454239516
Joshdb-18 <joshuadami17@gmail.com>
2023-05-04 09:48:32 +0100

    Country users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- If the table already exists, this script will not fail
+CREATE TABLE IF NOT EXISTS users (
+  -- id column is an integer, never null, auto increment and primary key
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  -- email column is a string of maximum 255 characters, never null, and unique
+  email VARCHAR(255) NOT NULL UNIQUE,
+  -- name column is a string of maximum 255 characters
+  name VARCHAR(255),
+  -- country column is an enumeration of countries: US, CO, and TN, never null (default is US)
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
