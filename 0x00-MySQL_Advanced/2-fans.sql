28d6e111f237c5a910171b5ff7d77bb548217e47
Joshdb-18 <joshuadami17@gmail.com>
2023-05-04 10:00:48 +0100

    Calculate/compute something is always power intensive… better to distribute the load!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -3,7 +3,7 @@ SELECT origin, COUNT(*) AS nb_fans
 -- From the bands table
 FROM bands
 -- Join with the fans table to get the number of fans for each band
-JOIN fans ON bands.id = fans.band_id
+JOIN fans ON bands.band_id = fans.band_id
 -- Group by origin to aggregate the number of fans for each country
 GROUP BY origin
 -- Order by the number of fans in descending order
