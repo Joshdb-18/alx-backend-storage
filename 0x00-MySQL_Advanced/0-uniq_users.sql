343a72bcad45c6fbc8e59997642fb2a06f54a9c9
Joshdb-18 <joshuadami17@gmail.com>
2023-05-04 10:08:04 +0100

    Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,10 @@
+-- Creates table users
 -- If the table already exists, this script will not fail
+-- id column is an integer, never null, auto increment and primary key
+-- email column is a string of maximum 255 characters, never null, and unique
+-- name column is a string of maximum 255 characters
 CREATE TABLE IF NOT EXISTS users (
-  -- id column is an integer, never null, auto increment and primary key
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  -- email column is a string of maximum 255 characters, never null, and unique
   email VARCHAR(255) NOT NULL UNIQUE,
-  -- name column is a string of maximum 255 characters
   name VARCHAR(255)
 );
